59975717c777b4e44444a7c1d4195dc5044aa173
Javierub32 <124401821+Javierub32@users.noreply.github.com>
2023-02-04 01:08:12 +0100

    Add files via upload

---
+++ Plantilla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    system("pause");
+    return 0;
+}
+
+/*
+Ctr + Ñ --> Abrir terminal
+cls --> Limpiar terminal
+Ctr + Shift + P + "CodeSnap" --> Captura de pantalla
+Ctrl + Mayús+ flecha abajo --> Seleccionar una línea
+*/
